b89f855b3d932ea328c710487d876d7e50eb047d
Ken Tsang <kentsang@Kens-MacBook-Pro.local>
2020-11-25 01:29:40 +0000

    Fix lint in test

--- tests/test_dates.py
+++ tests/test_dates.py
@@ -7,6 +7,7 @@ class MockEventDates:
     def __init__(self, event_datetime):
         self.event_datetime = datetime.strptime(event_datetime, "%Y-%m-%d %H:%M")
 
+
 def test_get_nice_event_dates():
     event_dates = [
         MockEventDates('2019-01-01 12:00'),
@@ -15,6 +16,7 @@ def test_get_nice_event_dates():
     ]
     nice_event_dates = get_nice_event_dates(event_dates)
     assert nice_event_dates == 'Tue 1, Tue 15 of January, Tue 5 of February - 12 PM'
+
 
 def test_get_nice_event_dates_with_dict():
     event_dates = [
