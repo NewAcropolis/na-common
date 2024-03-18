a7cece28f0f29da27715e08232446ed4915c5b96
Ken Tsang <kenlt.uk@gmail.com>
2024-03-18 01:41:47 +0000

    Update UK delivery fee to 3.50

--- na_common/delivery.py
+++ na_common/delivery.py
@@ -1,17 +1,17 @@
 DELIVERY_ZONES = [
     {
-        'name': 'UK', 'codes': 'GB', 'price': 2.00
+        'name': 'UK', 'codes': 'GB', 'price': 3.50
     }, {
         # https://www.royalmail.com/international-zones#europe
         'name': 'Europe',
         'codes': 'AL,AD,AM,AT,AZ,BY,BE,BA,BG,HR,CY,CZ,DK,EE,FO,FI,FR,GE,DE,GI,GR,GL,HU,IS,IE,IT,'
                  'KZ,KG,LV,LI,LT,LU,MK,MT,MD,MC,ME,NL,NO,PL,PT,RO,RU,SM,RS,SK,ES,SE,CH,TJ,TR,TM,'
                  'UA,UZ,VA',
-        'price': 4.50
+        'price': 7.50
     }, {
         'name': 'RoW',
         'codes': '',
-        'price': 6.00
+        'price': 10.00
     }
 ]
 
